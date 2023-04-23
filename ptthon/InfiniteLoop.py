a2e086380fcce59a2bd99c8cb93b2be75d21ace9
Avril <131004724+argentum1113@users.noreply.github.com>
2023-04-23 20:14:18 +0800

    Update InfiniteLoop.py

--- ptthon/InfiniteLoop.py
+++ ptthon/InfiniteLoop.py
@@ -1,6 +1,6 @@
 """
 File: InfiniteLoop.py
-Name: Jerry Liao
+Name: AVRIL
 ------------------------
 This file shows the idea of infinite loop,
 reminding students one of the 3 bugs in using
@@ -9,14 +9,12 @@ while loop
 
 from karel.stanfordkarel import *
 
-
 def main():
     """
     Karel will be turning left non-stop
     """
     while front_is_clear():
         turn_left()
-
 
 # ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
 if __name__ == '__main__':
